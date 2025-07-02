18de65de1213e10fe1d35f4e8eec0a45222c13ed
Helder S Ribeiro <helder@agentofuser.com>
2025-07-02 17:20:16 -0400

    WIP: add aunt relation

--- python/aunt-kg.py
+++ python/aunt-kg.py
@@ -2,43 +2,143 @@ from client import MORK, ManagedMORK
 
 
 DATASETS = (
-    "royal92",
+    # "royal92",
     "lordOfTheRings",
-    "adameve",
-    "simpsons",
+    # "adameve",
+    # "simpsons",
 )
+
 
 def preprocessing(server, datasets=DATASETS):
     with server.work_at("aunt-kg") as ins:
         for dataset in datasets:
             with ins.work_at(dataset) as scope:
                 with scope.work_at("src") as src:
-                    src.sexpr_import_(f"https://raw.githubusercontent.com/Adam-Vandervorst/metta-examples/refs/heads/main/aunt-kg/{dataset}.metta")\
-                        .block()
-                    downloaded = src.download("(Individuals $i (Fullname $name))", "$name")
+                    src.sexpr_import_(
+                        f"https://raw.githubusercontent.com/Adam-Vandervorst/metta-examples/refs/heads/main/aunt-kg/{dataset}.metta"
+                    ).block()
+                    downloaded = src.download(
+                        "(Individuals $i (Fullname $name))", "$name"
+                    )
                     print("names", dataset, downloaded.data)
 
-                scope.transform(("(src (Individuals $i (Id $id)))", "(src (Individuals $i (Fullname $name)))"), ("(simple (hasName $id $name))", "(simple (hasId $name $id))"))
+                scope.transform(
+                    (
+                        "(src (Individuals $i (Id $id)))",
+                        "(src (Individuals $i (Fullname $name)))",
+                    ),
+                    ("(simple (hasName $id $name))", "(simple (hasId $name $id))"),
+                )
 
-                scope.transform(("(src (Individuals $i (Id $id)))", "(src (Individuals $i (Sex \"M\")))"), ("(simple (male $id))",))
-                scope.transform(("(src (Individuals $i (Id $id)))", "(src (Individuals $i (Sex \"F\")))"), ("(simple (female $id))",))
+                scope.transform(
+                    (
+                        "(src (Individuals $i (Id $id)))",
+                        '(src (Individuals $i (Sex "M")))',
+                    ),
+                    ("(simple (male $id))",),
+                )
+                scope.transform(
+                    (
+                        "(src (Individuals $i (Id $id)))",
+                        '(src (Individuals $i (Sex "F")))',
+                    ),
+                    ("(simple (female $id))",),
+                )
 
-                scope.transform(("(src (Relations $r (Husband $id)))", "(src (Relations $r (Children $lci $cid)))"), ("(simple (parent $id $cid))",))\
-                    .block()
-                scope.transform(("(src (Relations $r (Wife $id)))", "(src (Relations $r (Children $lci $cid)))"), ("(simple (parent $id $cid))",))
-                scope.transform(("(src (Relations $r (Husband $id)))", "(src (Relations $r (Children $cid)))"), ("(simple (parent $id $cid))",))\
-                    .block()
-                scope.transform(("(src (Relations $r (Wife $id)))", "(src (Relations $r (Children $cid)))"), ("(simple (parent $id $cid))",))
+                scope.transform(
+                    (
+                        "(src (Relations $r (Husband $id)))",
+                        "(src (Relations $r (Children $lci $cid)))",
+                    ),
+                    ("(simple (parent $id $cid))",),
+                ).block()
+                scope.transform(
+                    (
+                        "(src (Relations $r (Wife $id)))",
+                        "(src (Relations $r (Children $lci $cid)))",
+                    ),
+                    ("(simple (parent $id $cid))",),
+                )
+                scope.transform(
+                    (
+                        "(src (Relations $r (Husband $id)))",
+                        "(src (Relations $r (Children $cid)))",
+                    ),
+                    ("(simple (parent $id $cid))",),
+                ).block()
+                scope.transform(
+                    (
+                        "(src (Relations $r (Wife $id)))",
+                        "(src (Relations $r (Children $cid)))",
+                    ),
+                    ("(simple (parent $id $cid))",),
+                )
 
-    ins.sexpr_export("($dataset (simple $x))", "($dataset $x)", "file://" + __file__.rpartition("/")[0] + "/simple_all.metta")
+                # Add mother relations
+                scope.transform(
+                    (
+                        "(simple (parent $pid $cid))",
+                        "(simple (female $pid))",
+                    ),
+                    ("(simple (mother $pid $cid))",),
+                ).block()
+
+                # Add sister relations
+                scope.transform(
+                    (
+                        "(simple (parent $pid $cid0))",
+                        "(simple (parent $pid $cid1))",
+                        "(simple (female $cid0))",
+                    ),
+                    ("(simple (sister $cid0 $cid1))",),
+                )
+
+                ## Add byName sister relations
+                scope.transform(
+                    (
+                        "(simple (sister $cid0 $cid1))",
+                        "(simple (hasName $cid0 $name0))",
+                        "(simple (hasName $cid1 $name1))",
+                    ),
+                    ("(simple (sisterByName $name0 $name1))",),
+                ).block()
+
+                # Add aunt relations
+                scope.transform(
+                    (
+                        "(simple (parent $pid $cid))",
+                        "(simple (sister $aid $pid))",
+                    ),
+                    ("(simple (aunt $aid $cid))",),
+                ).block()
+
+                # Add byName aunt relations
+                scope.transform(
+                    (
+                        "(simple (aunt $aid $cid))",
+                        "(simple (hasName $aid $name0))",
+                        "(simple (hasName $cid $name1))",
+                    ),
+                    ("(simple (auntByName $name0 $name1))",),
+                ).block()
+
+    ins.sexpr_export(
+        "($dataset (simple $x))",
+        "($dataset $x)",
+        "file://" + __file__.rpartition("/")[0] + "/simple_all.metta",
+    )
 
     for i, item in enumerate(ins.history):
         print("preprocessing event", i, str(item))
 
+
 def _main():
-    with ManagedMORK.connect(binary_path="../target/release/mork_server").and_terminate() as server:
+    with ManagedMORK.connect(
+        binary_path="../target/release/mork_server"
+    ).and_terminate() as server:
         server.clear().block()
         preprocessing(server)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _main()
